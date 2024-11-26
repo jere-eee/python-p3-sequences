a5b7aa4b45967bcf1c31a8b1eb11650559cade35
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-26 09:41:33 +0300

    fibonacci passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    current_list = [0, 1]
+    
+    for n in range(2, length):
+        new_n = current_list[-1] + current_list[-2]
+        current_list.append(new_n)
+        
+    print(current_list)
